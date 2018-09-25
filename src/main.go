95df834b64ed9617a3c608c7a567aa814846b342
SirawichDev <sirawit0676@gmail.com>
2018-09-25 16:39:07 +0700

    init table with gorm create table

---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+var err error
+
+type User struct {
+	ID        uint
+	Username  string
+	FirstName string
+	LastName  string
+}
+
+func main() {
+	db, err := gorm.Open("postgres", "user=sirawich dbname=booker sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	dbase := db.DB()
+	defer dbase.Close()
+
+	err = dbase.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+	println("Connection to database established")
+	db.CreateTable(&User{})
+
+	println("Done!")
+}
