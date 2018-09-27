84e77ef5f3650d4ce98c7919600261fabab6bfc3
SirawichDev <sirawit0676@gmail.com>
2018-09-27 08:13:06 +0700

    show first and last data in table

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/CRUD-Gin-Gorm/Models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
@@ -8,11 +10,18 @@ import (
 
 var err error
 
+//Data sample
+var users []Models.User = []Models.User{
+	Models.User{Uname: "x1", Password: "1234", NickName: "xx"},
+	Models.User{Uname: "x2", Password: "3241", NickName: "xxx"},
+}
+
 func main() {
 	db, err := gorm.Open("postgres", "user=sirawich dbname=booker sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
+
 	defer db.Close()
 
 	dbase := db.DB()
@@ -23,14 +32,20 @@ func main() {
 		panic(err.Error())
 	}
 	println("Connection to database established")
-	User := &Models.User{}
-	db.CreateTable(User)
-	user := &Models.User{
-		Uname:    "sirawit0676",
-		Password: "1w2e3r4t5y",
-		NickName: "Ex",
+	db.CreateTable(&Models.User{})
+	for _, users := range users {
+		db.Create(&users)
 	}
-	db.Create(user)
-
+	// user := &Models.User{
+	// 	Uname:    "sirawit0676",
+	// 	Password: "1w2e3r4t5y",
+	// 	NickName: "Ex",
+	// }
+	u := Models.User{}
+	//show first data in table
+	// db.First(&u)
+	//show last data intable
+	db.Last(&u)
+	fmt.Println(u)
 	println("Done!")
 }
